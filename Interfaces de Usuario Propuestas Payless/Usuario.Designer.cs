77cf3bab94047b1a7e9d8f5df52862355cafe03e
Leonel Flores <leonelosejo426@gmail.com>
2026-04-23 15:00:53 -0600

    asento paar el boron menú

--- Interfaces de Usuario Propuestas Payless/Usuario.Designer.cs
+++ Interfaces de Usuario Propuestas Payless/Usuario.Designer.cs
@@ -544,11 +544,12 @@
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(1287, 118);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(110, 35);
             this.button4.TabIndex = 84;
-            this.button4.Text = "Menu";
+            this.button4.Text = "Menú";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
