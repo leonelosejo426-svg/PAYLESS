6e2d1877c72f67dc9362ab60f4ae85623ad5df05
Leonel Flores <leonelosejo426@gmail.com>
2026-04-28 15:37:51 -0600

    Finalizado el diseño del formulario de caja

--- Interfaces de Usuario Propuestas Payless/Caja.cs
+++ Interfaces de Usuario Propuestas Payless/Caja.cs
@@ -16,5 +16,10 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
