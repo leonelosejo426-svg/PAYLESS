36044c54aa08914b471d36ae731ed7360575b4d1
Leonel Flores <leonelosejo426@gmail.com>
2026-05-04 11:43:05 -0600

    Cambios en el diseño de ventas

--- Interfaces de Usuario Propuestas Payless/Ventas.cs
+++ Interfaces de Usuario Propuestas Payless/Ventas.cs
@@ -19,9 +19,7 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
 
         private void button4_Click(object sender, EventArgs e)
         {
-            Menú_Principal ventana = new Menú_Principal();
-            ventana.Show();
-            this.Hide();
+            
         }
     }
 }
