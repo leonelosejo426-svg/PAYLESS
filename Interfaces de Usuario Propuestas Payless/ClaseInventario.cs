255d6977152fffb066aada291203edfa6e577e5f
Leonel Flores <leonelosejo426@gmail.com>
2026-05-04 22:05:28 -0600

    Creacion de la clase Inventario

---
+++ Interfaces de Usuario Propuestas Payless/ClaseInventario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_de_Usuario_Propuestas_Payless
+{
+    internal class ClaseInventario
+    {
+
+        private int _idInventario;
+        private int _idProducto;
+        private int _stockActual;
+        private int _stockMinimo;
+
+    }
+}
