4cdee0b3ed132cf291937601a972fd32aaa94643
Yubelkis paola <yubelkispaola194@gmail.com>
2026-06-03 16:44:20 -0600

    creaccion de pantalla recuperacion de datos de la base de datos

---
+++ Interfaces de Usuario Propuestas Payless/Respaldo_de_datos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaces_de_Usuario_Propuestas_Payless
+{
+    public partial class Respaldo_de_datos : Form
+    {
+        public Respaldo_de_datos()
+        {
+            InitializeComponent();
+        }
+
+        private void Respaldo_de_datos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
