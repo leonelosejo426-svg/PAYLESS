a58be4f5760c13129bc1a0085b44822b63d48cc7
felip <felip@FELIPE2004>
2026-06-11 13:04:38 -0600

    modificaciones de la pantalla de Reporte compra

--- Interfaces de Usuario Propuestas Payless/ReporteCompra.cs
+++ Interfaces de Usuario Propuestas Payless/ReporteCompra.cs
@@ -29,7 +29,7 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
 
         private void button5_Click(object sender, EventArgs e)
         {
-            Compras = new Compras();
+            Compras ventana = new Compras();
             ventana.Show();
             this.Hide();
         }
