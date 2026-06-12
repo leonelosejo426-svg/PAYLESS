9b5ab6fe72689ed2d20348957c196289593fb74e
felip <felip@FELIPE2004>
2026-06-12 13:59:54 -0600

    modificaciones de los codigos de la pnatalla Productos

--- Interfaces de Usuario Propuestas Payless/Productos.cs
+++ Interfaces de Usuario Propuestas Payless/Productos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,59 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
 {
     public partial class Productos : Form
     {
+
+        List<Producto> listaProductos = new List<Producto>();
+
+        int indiceEditar = -1;
+
+
         ClaseUsuario usuarioActual;
         public Productos()
         {
             InitializeComponent();
+        }
+
+        public class Producto
+{
+    public string Codigo { get; set; }
+
+    public string NombreProducto { get; set; }
+
+    public string Categoria { get; set; }
+
+    public string Medida { get; set; }
+
+    public string Marca { get; set; }
+
+    public string Proveedor { get; set; }
+
+    public DateTime FechaRegistro { get; set; }
+}
+
+
+        private void GuardarJSON()
+        {
+            string json = JsonConvert.SerializeObject(
+                listaProductos,
+                Formatting.Indented);
+
+            File.WriteAllText("productos.json", json);
+        }
+
+        private void MostrarProductos()
+        {
+            dgvProductos.DataSource = null;
+            dgvProductos.DataSource = listaProductos;
+        }
+        private void Limpiar()
+        {
+            txtCodigo.Clear();
+            txtNombreProducto.Clear();
+
+            cmbCategoria.SelectedIndex = -1;
+            cmbMedida.SelectedIndex = -1;
+            cmbMarca.SelectedIndex = -1;
+            cmbProveedor.SelectedIndex = -1;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -25,11 +76,28 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
 
         private void Productos_Load(object sender, EventArgs e)
         {
+            
+            
             if (ClaseSesion.RolActual != "ADMIN")
             {
                 MessageBox.Show("No tienes acceso");
                 this.Hide();
             }
+
+
+
+            if (File.Exists("productos.json"))
+            {
+                string json = File.ReadAllText("productos.json");
+
+                listaProductos = JsonConvert.DeserializeObject<List<Producto>>(json);
+
+                MostrarProductos();
+            }
+
+            cmbBuscarPor.Items.Add("Producto");
+            cmbBuscarPor.Items.Add("Fecha más reciente");
+            cmbBuscarPor.Items.Add("Fecha más antigua");
 
 
             this.Size = new Size(1250, 700);
@@ -107,6 +175,136 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
         private void label9_Click_1(object sender, EventArgs e)
         {
             Mantenimiento ventana = new Mantenimiento(); ventana.Show(); this.Hide();
+        }
+
+        private void cmbBuscarPor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbBuscarPor.Items.Add("Producto");
+            cmbBuscarPor.Items.Add("Fecha más reciente");
+            cmbBuscarPor.Items.Add("Fecha más antigua");
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (indiceEditar >= 0)
+            {
+                // ACTUALIZAR
+                listaProductos[indiceEditar].Codigo = txtCodigo.Text;
+                listaProductos[indiceEditar].NombreProducto = txtNombreProducto.Text;
+                listaProductos[indiceEditar].Categoria = cmbCategoria.Text;
+                listaProductos[indiceEditar].Medida = cmbMedida.Text;
+                listaProductos[indiceEditar].Marca = cmbMarca.Text;
+                listaProductos[indiceEditar].Proveedor = cmbProveedor.Text;
+
+                indiceEditar = -1;
+
+                MessageBox.Show("Producto actualizado.");
+            }
+            else
+            {
+                // AGREGAR NUEVO
+                Producto p = new Producto();
+
+                p.Codigo = txtCodigo.Text;
+                p.NombreProducto = txtNombreProducto.Text;
+                p.Categoria = cmbCategoria.Text;
+                p.Medida = cmbMedida.Text;
+                p.Marca = cmbMarca.Text;
+                p.Proveedor = cmbProveedor.Text;
+                p.FechaRegistro = DateTime.Now;
+
+                listaProductos.Add(p);
+
+                MessageBox.Show("Producto agregado.");
+            }
+
+            MostrarProductos();
+            GuardarJSON();
+            Limpiar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(
+        listaProductos,
+        Formatting.Indented);
+
+            File.WriteAllText("productos.json", json);
+
+            MessageBox.Show("Productos guardados correctamente.");
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (cmbBuscarPor.Text == "Producto")
+            {
+                dgvProductos.DataSource = null;
+
+                dgvProductos.DataSource =
+                    listaProductos
+                    .OrderBy(x => x.NombreProducto)
+                    .ToList();
+            }
+            if (cmbBuscarPor.Text == "Fecha más reciente")
+            {
+                dgvProductos.DataSource = null;
+
+                dgvProductos.DataSource =
+                    listaProductos
+                    .OrderByDescending(x => x.FechaRegistro)
+                    .ToList();
+            }
+            if (cmbBuscarPor.Text == "Fecha más antigua")
+            {
+                dgvProductos.DataSource = null;
+
+                dgvProductos.DataSource =
+                    listaProductos
+                    .OrderBy(x => x.FechaRegistro)
+                    .ToList();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvProductos.CurrentRow != null)
+            {
+                indiceEditar = dgvProductos.CurrentRow.Index;
+
+                Producto p = listaProductos[indiceEditar];
+
+                txtCodigo.Text = p.Codigo;
+                txtNombreProducto.Text = p.NombreProducto;
+                cmbCategoria.Text = p.Categoria;
+                cmbMedida.Text = p.Medida;
+                cmbMarca.Text = p.Marca;
+                cmbProveedor.Text = p.Proveedor;
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvProductos.CurrentRow != null)
+            {
+                int indice = dgvProductos.CurrentRow.Index;
+
+                DialogResult respuesta = MessageBox.Show(
+                    "¿Desea eliminar este producto?",
+                    "Confirmar",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (respuesta == DialogResult.Yes)
+                {
+                    listaProductos.RemoveAt(indice);
+
+                    MostrarProductos();
+
+                    btnGuardar.PerformClick();
+
+                    MessageBox.Show("Producto eliminado.");
+                }
+            }
         }
     }
 }
