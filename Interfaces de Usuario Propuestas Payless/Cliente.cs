8beb5250a260403c71cadade582a243d34ade86b
Yubelkis paola <yubelkispaola194@gmail.com>
2026-06-12 19:31:32 -0600

    pantalla cliente

--- Interfaces de Usuario Propuestas Payless/Cliente.cs
+++ Interfaces de Usuario Propuestas Payless/Cliente.cs
@@ -358,7 +358,7 @@ namespace Interfaces_de_Usuario_Propuestas_Payless
                 Estado = CBestado.Text
             };
 
-            listacliente.Add(nuevoCliente);
+            listacliente.Add(nuevocliente);
 
             DGVtabla1.DataSource = null;
             DGVtabla1.DataSource = listacliente;
