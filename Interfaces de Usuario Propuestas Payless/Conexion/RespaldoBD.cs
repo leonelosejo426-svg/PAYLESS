c7620b32cbc6c63bfb70dbc9fc65ab0e373aa248
Leonel Flores <leonelosejo426@gmail.com>
2026-08-11 09:30:53 -0600

    clase respaldo

---
+++ Interfaces de Usuario Propuestas Payless/Conexion/RespaldoBD.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Interfaces_de_Usuario_Propuestas_Payless.Conexion
+{
+    internal class RespaldoBD
+    {
+
+       /* private Conexion conexionBD;
+        private string rutaCarpetaRespaldos;
+
+        public RespaldoBD()
+        {
+            conexionBD = new Conexion();
+        //La caprpeta Respaldo  se crea automaticamente en el directorio de ejecucion.exe
+        rutaCarpetaRespaldos = Phat.Combine(AppDomain.CurrentDomain.BaseDirectory, "Respaldos");
+
+            if(!Directory.Exists(rutaCarpetaRespaldos))
+            {
+                Directory.CreateDirectory(rutaCarpetaRespaldos);
+            }
+        }
+
+        //Create un respaldo completo de l BD en un archibo .sql
+        public bool CrearRespaldo(string nombrePerzonalizado, out string rutaArchivoFinal)
+        {
+            rutaCarpetaRespaldos = string.Empty
+                try
+            {
+                //formato de nombre Nombre_2026-08-02_20-09-22.sql
+                string nombreArchivo = $"{nombrePerzonalizado}_{DataTime.Now:yyyy-MM-dd_HH-mm-ss}.sql";
+                rutaArchivoFinal = Path.Combine(rutaCarpetaRespaldos, nombreArchivo);
+
+                using NpgsqlConnection
+
+            }
+       
+        }
+
+    */
+
+
+    }
+}
